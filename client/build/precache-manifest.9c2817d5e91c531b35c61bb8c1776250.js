67fcdabd5f4911eef994a5d15c1e57501e850ced
Vidit Jain <84613629+ViditJain123@users.noreply.github.com>
2023-11-28 21:34:48 +0530

    final code
    complete

---
+++ client/build/precache-manifest.9c2817d5e91c531b35c61bb8c1776250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a1b6fbbcb81e34647fe0be8b0dad60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc43d15923910713c84",
+    "url": "/static/css/main.8fff4483.chunk.css"
+  },
+  {
+    "revision": "baf7b47fc34768755efd",
+    "url": "/static/js/2.0328b64c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0328b64c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc43d15923910713c84",
+    "url": "/static/js/main.4cf09e25.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
